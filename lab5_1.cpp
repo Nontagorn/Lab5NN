39ca978ab1808a4c8615bb5c31881348ab865f2c
PondD <pangpon_2542@hotmail.com>
2020-12-15 16:11:51 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int i ;
+    int Even = 0;
+    int Odd = 0;
+    while(i != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> i;
+    if(i == 0){
+     break;
+    }
+    else if(i%2 == 0){
+        Even += 1;
+    }
+    else{
+        Odd += 1;
+    }
+    }
+    cout << "#Even numbers = " << Even << "\n";
+    cout << "#Odd numbers = " << Odd;
     return 0;
 }
